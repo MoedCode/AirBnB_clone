6225fc2a22951ed2387e3feb5f203dc4f6d890b0
MoedCode <mohamed71291@gmail.com>
2023-11-09 21:45:56 +0200

    AirBnB_clone  frst code (automated command)

--- console.py
+++ console.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""
+    In the AirBnB Clone Project, the "console" is a command-line
+    interface designed for managing and manipulating project objects,
+    facilitating tasks such as creating, updating, deleting, and retrieving data. Additionally, the console functions as a testing and validation tool for the project's storage engine, ensuring effective abstraction of data storage and retrieval.
+"""
+import cmd
+
+
+class CommandInterpreter(cmd.Cmd):
+    """ A commandline interpreter class"""
+
+    def do_quit(self, arg):
+        """Quit the program."""
+        return True
+    do_EOF = do_quit
+
+
+cmd = CommandInterpreter()
+cmd.cmdloop()
