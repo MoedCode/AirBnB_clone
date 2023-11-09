fdd4d335996253cec743caed1307ce94a62854a2
MoedCode <mohamed71291@gmail.com>
2023-11-09 05:24:36 +0200

    AirBnB_clone  pre coding 1 (automated command)

---
+++ tst.py
@@ -0,0 +1,26 @@
+# from cmd import Cmd
+import cmd
+
+
+class CommandLine(cmd.Cmd):
+    prompt = "$ "
+
+    def do_salam(self, arg):
+        """ quite / exit commands"""
+        return True
+    # aliasing 4Ex making another command for exit
+    # declaring variable do_exit then assign exit function to it
+    do_exit = do_salam
+    do_kudashi = do_salam
+    # all of those aliases pointing k same location
+
+
+command = CommandLine()
+command.cmdloop()
+
+# if __name__ == "__main__":
+#     inst1 = CommandLine
+#     print()
+#     print(f"cmd module: \n {dir(Cmd)}")
+#     print(f"cmd class:\n {dir(CommandLine)}")
+#     print(f"inst of cmd class:\n {dir(inst1)}")
