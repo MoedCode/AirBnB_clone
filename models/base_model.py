87a239c755e5aa01452dc2859bfbbd0f3ab4a7a1
MoedCode <mohamed71291@gmail.com>
2023-11-10 04:24:29 +0200

    AirBnB_clone  task3 (automated command)

--- models/base_model.py
+++ models/base_model.py
@@ -1,27 +1,57 @@
 #!/usr/bin/python3
 
-# models/base_model.py
+"""
+BaseModel Class for AirBnB Clone Project
+
+This module defines the BaseModel class for the AirBnB clone project. The BaseModel class
+serves as the parent class for other classes, providing common attributes and methods.
+
+Classes:
+    BaseModel: A class representing the base model for other classes in the project.
+
+Attributes:
+    - id (str): A unique identifier generated using the uuid module.
+    - created_at (datetime): The datetime when an instance is created.
+    - updated_at (datetime): The datetime when an instance is created and updated.
+
+Methods:
+    - __init__: Initializes a new instance of the BaseModel class.
+    - __str__: Returns a string representation of the BaseModel instance.
+    - save: Updates the 'updated_at' attribute with the current datetime.
+    - to_dict: Converts the BaseModel instance to a dictionary representation.
+
+Usage:
+    This module can be used standalone to create an instance of BaseModel and print its dictionary representation.
+"""
+
 import uuid
 from datetime import datetime
 
 
 class BaseModel:
     def __init__(self):
-        self.id = str(uuid.uuid4())
+        """Initialize a new instance of the BaseModel class."""
+        self.id = str(uuid.uuid4())  # Generate a unique ID
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
-        return "[{}] ({}) {}".format(
-            self.__class__.__name__, self.id, self.__dict__)
+        """Return a string representation of the BaseModel instance."""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
+        """Update the 'updated_at' attribute with the current datetime."""
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """
+        Convert the BaseModel instance to a dictionary representation.
+
+        Returns:
+            dict: A dictionary containing all keys/values of the instance.
+                  Includes '__class__', 'created_at', and 'updated_at' keys.
+        """
         obj_dict = self.__dict__.copy()
-        print("to_dict", obj_dict)
-        print("self.dict", self.__dict__)
         obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
@@ -30,4 +60,6 @@ class BaseModel:
 
 
 if __name__ == "__main__":
+    # Create an instance of BaseModel and print its dictionary representation
     bm = BaseModel().to_dict()
+    print(bm)
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,32 +1,58 @@
-# tests/test_models/test_base_model.py
+#!/usr/bin/python3
+
+"""
+Test Module for BaseModel Class
+
+This module contains unit tests for the BaseModel class in the AirBnB clone project.
+It uses the unittest framework to test various aspects of the BaseModel class.
+
+Classes:
+    TestBaseModel: A class containing test cases for the BaseModel class.
+
+Test Cases:
+    - test_instance_creation: Tests the creation of a BaseModel instance.
+    - test_attributes: Tests the presence of required attributes in a BaseModel instance.
+    - test_str_method: Tests the __str__ method of a BaseModel instance.
+    - test_save_method: Tests the save method of a BaseModel instance.
+    - test_to_dict_method: Tests the to_dict method of a BaseModel instance.
+
+Usage:
+    Run this module directly to execute all the defined tests.
+"""
+
 import unittest
 from models.base_model import BaseModel
 
 
 class TestBaseModel(unittest.TestCase):
     def test_instance_creation(self):
+        """Test the creation of a BaseModel instance."""
         my_model = BaseModel()
         self.assertIsInstance(my_model, BaseModel)
 
     def test_attributes(self):
+        """Test the presence of required attributes in a BaseModel instance."""
         my_model = BaseModel()
         self.assertTrue(hasattr(my_model, 'id'))
         self.assertTrue(hasattr(my_model, 'created_at'))
         self.assertTrue(hasattr(my_model, 'updated_at'))
 
     def test_str_method(self):
+        """Test the __str__ method of a BaseModel instance."""
         my_model = BaseModel()
         str_representation = str(my_model)
         self.assertIn("[BaseModel] ({})".format(
             my_model.id), str_representation)
 
     def test_save_method(self):
+        """Test the save method of a BaseModel instance."""
         my_model = BaseModel()
         initial_updated_at = my_model.updated_at
         my_model.save()
         self.assertNotEqual(initial_updated_at, my_model.updated_at)
 
     def test_to_dict_method(self):
+        """Test the to_dict method of a BaseModel instance."""
         my_model = BaseModel()
         obj_dict = my_model.to_dict()
         self.assertIsInstance(obj_dict, dict)
