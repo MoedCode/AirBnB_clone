e15244962452759e9f50c8907ab3824edc04df8f
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-11-11 19:22:09 +0200

    some modifications (A.C)

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -29,15 +29,24 @@ class FileStorage:
 
             json.dump(serialized_objects, file)
 
-    def reload2(self):
+    def reload(self):
         # deserializes the JSON file to __objects (only if the JSON file (__file_path) exists ;
         # otherwise, do nothing. If the file doesn’t exist, no exception should be raised)
 
-        if os.path.exists(self.__file_path) and os.stat(self.__file_path).st_size > 0:
-            with open(self.__file_path, "r") as file:
-                self.__objects = json.load(file)
+        # if os.path.exists(self.__file_path) and os.stat(self.__file_path).st_size > 0:
+        #     with open(self.__file_path, "r") as file:
+        #         self.__objects = json.load(file)
+        with open(self.__file_path, "r") as file:
+            loaded_data = json.loads(file.read())
+            for k, obj_dict in loaded_data.items():
+                class_name = obj_dict.get("__class__")
 
-    def reload(self):
+                if class_name in FileStorage.__classes:
+                    current_class = FileStorage.__classes[class_name]
+                    instance = current_class(**obj_dict)
+                    FileStorage.__objects[k] = instance
+
+    def reload3(self):
         try:
             with open(self.__file_path, "r") as file:
                 dictionary = json.loads(file.read())
