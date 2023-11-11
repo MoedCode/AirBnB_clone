f8eb69e140045c3860016d3aa7b5df4483075d82
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-11-11 21:29:01 +0200

    first commit (A.C)

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -38,27 +38,7 @@ class FileStorage:
 
             json.dump(serialized_objects, file)
 
-    def reload1(self):
-        """doc"""
-        # deserializes the JSON file to __objects (only if the JSON file (__file_path) exists ;
-        # otherwise, do nothing. If the file doesn’t exist, no exception should be raised)
-
-        # if os.path.exists(self.__file_path) and os.stat(self.__file_path).st_size > 0:
-        #     with open(self.__file_path, "r") as file:
-        #         self.__objects = json.load(file)
-        with open(self.__file_path, "r") as file:
-            loaded_data: dict = json.loads(file.read())
-
-            for key, obj_dict in loaded_data.items():
-                class_name = obj_dict.get("__class__")
-
-                if class_name in FileStorage.__classes:
-                    current_class: FileStorage = FileStorage.__classes[class_name]
-                    instance = current_class(**obj_dict)
-                    FileStorage.__objects[key] = instance
-
     def reload(self):
-        """doc"""
         try:
             with open(self.__file_path, "r") as file:
                 dictionary = json.loads(file.read())
@@ -67,3 +47,33 @@ class FileStorage:
                     self.new(eval(cls_name)(**value))
         except Exception:
             pass
+
+    # def reload(self):
+    #     """doc"""
+    #     try:
+    #         with open(self.__file_path, "r") as file:
+    #             dictionary = json.loads(file.read())
+    #             for value in dictionary.values():
+    #                 cls_name = value["__class__"]
+    #                 self.new(eval(cls_name)(**value))
+    #     except Exception:
+    #         pass
+
+    # def reload1(self):
+    #     """doc"""
+    #     # deserializes the JSON file to __objects (only if the JSON file (__file_path) exists ;
+    #     # otherwise, do nothing. If the file doesn’t exist, no exception should be raised)
+
+    #     # if os.path.exists(self.__file_path) and os.stat(self.__file_path).st_size > 0:
+    #     #     with open(self.__file_path, "r") as file:
+    #     #         self.__objects = json.load(file)
+    #     with open(self.__file_path, "r") as file:
+    #         loaded_data: dict = json.loads(file.read())
+
+    #         for key, obj_dict in loaded_data.items():
+    #             class_name = obj_dict.get("__class__")
+
+    #             if class_name in FileStorage.__classes:
+    #                 current_class: FileStorage = FileStorage.__classes[class_name]
+    #                 instance = current_class(**obj_dict)
+    #                 FileStorage.__objects[key] = instance
