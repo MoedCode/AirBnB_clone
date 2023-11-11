6ced2a68e2bad6cb82b83b32d8211f6aa146538c
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-11-11 20:49:54 +0200

    first commit (A.C)

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,25 +1,34 @@
 #!/usr/bin/python3
 import json
 import os.path
+from models.base_model import BaseModel
 
 
 class FileStorage:
     __file_path: str = "file.json"
     __objects: dict = {}
+    __classes: dict = {"BaseModel": BaseModel}
 
     def __init__(self):
+        """doc"""
         pass
 
     def all(self):
+        """doc"""
+
         # returns the dictionary __objects
         return self.__objects
 
     def new(self, obj):
+        """doc"""
+
         # sets in __objects the obj with key <obj class name>.id
         # self.__objects = obj
         self.__objects[obj.__class__.__name__ + "." + obj.id] = obj
 
     def save(self):
+        """doc"""
+
         # serializes __objects to the JSON file (path: __file_path)
 
         with open(self.__file_path, "w") as file:
@@ -29,15 +38,27 @@ class FileStorage:
 
             json.dump(serialized_objects, file)
 
-    def reload2(self):
+    def reload(self):
+        """doc"""
         # deserializes the JSON file to __objects (only if the JSON file (__file_path) exists ;
         # otherwise, do nothing. If the file doesn’t exist, no exception should be raised)
 
-        if os.path.exists(self.__file_path) and os.stat(self.__file_path).st_size > 0:
-            with open(self.__file_path, "r") as file:
-                self.__objects = json.load(file)
+        # if os.path.exists(self.__file_path) and os.stat(self.__file_path).st_size > 0:
+        #     with open(self.__file_path, "r") as file:
+        #         self.__objects = json.load(file)
+        with open(self.__file_path, "r") as file:
+            loaded_data: dict = json.loads(file.read())
 
-    def reload(self):
+            for key, obj_dict in loaded_data.items():
+                class_name = obj_dict.get("__class__")
+
+                if class_name in FileStorage.__classes:
+                    current_class: FileStorage = FileStorage.__classes[class_name]
+                    instance = current_class(**obj_dict)
+                    FileStorage.__objects[key] = instance
+
+    def reload3(self):
+        """doc"""
         try:
             with open(self.__file_path, "r") as file:
                 dictionary = json.loads(file.read())
